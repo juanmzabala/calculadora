03c5fbfff0383715b4d28128835003a8f5ffc584
juanmzabala <juanmzabala@gmail.com>
2023-02-21 21:21:37 -0300

    Actualiza funciones
    Se simplificó a una sola función exportada toda la parte de cálculos matemáticos.

--- operaciones.js
+++ operaciones.js
@@ -1,19 +1,45 @@
-export function suma(a,b){
+function suma(a,b){
     return Number(a)+ Number(b)
 };
 
-export function resta(a,b){
+function resta(a,b){
     return Number(a)- Number(b)
 };
 
-export function division(a,b){
+function division(a,b){
     if(Number(b)=== 0){
         return "Err"
     }else{
-        return Number(a)/ Number(b)
+        let resultado = (Number(a)/ Number(b)).toFixed(3);
+        let entero = Math.trunc(resultado);
+        if (((resultado-entero)/1 === 0)){
+            return entero;
+        }else{
+            return resultado;
+        }
+        
     }
 };
 
-export function multiplicacion(a,b){
+function multiplicacion(a,b){
     return Number(a) * Number(b)
 };
+
+export function elegirOperacion(a,b,operador){
+    let data;
+    switch (operador){
+        case "+":
+            data = suma(a ,b);
+            break;
+        case "-":
+            data= resta(a,b);
+            break;
+        case "x":
+            data=multiplicacion(a,b);
+            break;
+        case "%":
+            data=division(a,b);
+            break;
+    }
+    return data;
+}
